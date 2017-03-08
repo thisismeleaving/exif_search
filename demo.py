831694c589413318fdb97bacabdf60f3647f8d19
Berty <thisismeleaving@gmail.com>
2017-03-08 08:38:47 -0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,6 +5,11 @@ Get the list of image record data from S3 and queue up the celery tasks
 to process them.  The task to extract exif data is chained to each task that
 fetches the individual image.
 
+This demo expects two celery workers to be running.
+e.g.
+celery -A celery_app -Q http -n image_fetcher
+celery -A celery_app -Q data_processing -n exif_extractor
+
 """
 from operator import itemgetter
 from celery import chain
